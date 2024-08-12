ea1c862645d793e109394cc4fa9fb4b8b02d1a3b
Abel Tadesse <tadesseabel26@gmail.com>
2024-08-13 01:58:03 +0300

    task_3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    // Request to check API status
+    $.get("http://0.0.0.0:5001/api/v1/status/", function (data) {
+        if (data.status === "OK") {
+            $('#api_status').addClass('available');
+        } else {
+            $('#api_status').removeClass('available');
+        }
+    });
+
+    // Handle amenity selection
+    const selectedAmenities = {};
+
+    $('input[type="checkbox"]').change(function () {
+        const amenityId = $(this).attr('data-id');
+        const amenityName = $(this).attr('data-name');
+
+        if ($(this).is(':checked')) {
+            selectedAmenities[amenityId] = amenityName;
+        } else {
+            delete selectedAmenities[amenityId];
+        }
+
+        const amenitiesList = Object.values(selectedAmenities).join(', ');
+        $('.amenities h4').text(amenitiesList);
+    });
+});
+
